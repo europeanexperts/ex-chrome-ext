60e34f42da65813006196fb03512eb82f3585115
nazarovits <istvan.nazarovits@gmail.com>
2017-05-25 09:06:42 +0200

    added loadURL method parse methods are wrapped into onLoad functions

--- public/js/contentScript.js
+++ public/js/contentScript.js
@@ -1,10 +1,15 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.method === 'jobs') {
-        sendResponse({data: SOCIAL_PARSER.parseJobs(), method: "runtime", _pre: request});
+        SOCIAL_PARSER.onLoadJobs(function () {
+            sendResponse({data: SOCIAL_PARSER.parseJobs(), method: "runtime", _pre: request});
+        });
     } else if (request.method === 'profile') {
-        SOCIAL_PARSER.onLoadProfile(function() {
+        SOCIAL_PARSER.onLoadProfile(function () {
             sendResponse({data: SOCIAL_PARSER.parseProfile(), method: "runtime", _pre: request});
         });
+    } else if (request.method = 'loadURL') {
+        SOCIAL_PARSER.loadURL(request.url);
+        sendResponse({data: 'OK', method: "runtime", _pre: request});
     } else {
         sendResponse({data: 'DEFAULT', method: "runtime",_pre: request});
     }
