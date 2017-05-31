f94e99b674c45820216c476c1aa44531550abed0
nazarovits <istvan.nazarovits@gmail.com>
2017-05-31 08:29:50 +0200

    Added "Hunter Profile" button

--- public/js/parser.js
+++ public/js/parser.js
@@ -2,6 +2,12 @@ window.SOCIAL_PARSER = window.SOCIAL_PARSER || {};
 
 (function () {
     'use strict';
+    var AUTH_TOKEN_KEY = 'euex-auth-token';
+
+    var PATTERNS = {
+        //PROFILE_URL: /^https:\/\/www\.linkedin\.com\/in\//
+        PROFILE_URL: /^\/in\//
+    };
 
     var SELECTORS = {
         TOTAL                 : '.search-results__total',
@@ -192,9 +198,185 @@ window.SOCIAL_PARSER = window.SOCIAL_PARSER || {};
     SOCIAL_PARSER.loadURL = function (url) {
         window.location.href = url;
     };
+
+    SOCIAL_PARSER.testProfilePage = function() {
+        var url = window.location.href;
+
+        return PATTERNS.PROFILE_URL.test(url);
+    };
+
+    // profile hunter:
+    function ProfileHunter () {
+        var instance = ProfileHunter.instance;
+
+        if (instance !== undefined) {
+            instance.renderButton();
+
+            return instance;
+        }
+
+        instance = this;
+        instance.init();
+
+        ProfileHunter.instance = instance;
+        return instance;
+    }
+
+    ProfileHunter.prototype = {
+        init: function() {
+            console.log('>>> init profile hunter');
+            this.render();
+
+            //this.$btn.on('click', $.proxy(this.onHunterClick, this));
+        },
+
+        getAuthToken: function() {
+            return localStorage.getItem(AUTH_TOKEN_KEY);
+        },
+
+        sendProfile: function(options, callback) {
+            var profile = options.profile;
+            var authToken = this.getAuthToken();
+
+            if (!authToken) {
+                this.notification({message: '<b>Unauthorized!</b> You must to log in into Chrome Extension.'});
+            }
+
+            $.ajax({
+                url        : 'http://euex-stage.fpdev.xyz/api/import/consultants',
+                method     : 'POST',
+                headers    : {
+                    'X-Authorization': authToken
+                },
+                crossDomain: true,
+                contentType: 'application/json',
+                accepts    : 'json',
+                data       : JSON.stringify(profile),
+                success    : function (res) {
+                    callback(null, res);
+                },
+                error      : function (err) {
+                    callback(err);
+                }
+            });
+        },
+
+        onHunterClick: function() {
+            var $body = $('body');
+            var self = this;
+
+            $body.animate({scrollTop: 0});
+            $body.animate({scrollTop: $('.profile-detail').height()}, 3000, function () {
+                var data = SOCIAL_PARSER.parseProfile();
+
+                self.sendProfile({profile: data}, function(err, res) {
+                    if (err) {
+                        return self.errorHandler(err);
+                    }
+
+                    self.notification({message: 'The profile was successfully parsed and was exported to server.', type: 'success'});
+                    self.$btn.find('.btnText').html('Exported');
+                });
+            });
+        },
+
+        notification: function(options) {
+            var message = options.message || 'Something went wrong!';
+            var type = options.type || 'error';
+            var color = (type === 'success') ? '#0084bf' : 'red';
+            var timeout = options.timeout || 3000;
+            var $wrapper = this.$notificationsWrapper;
+            var $p = $('<p>' + message + '</p>');
+
+            $p.css({
+                'text-align': 'center',
+                color: color
+            });
+
+            $wrapper.css({
+                    'border-bottom': '5px solid ' + color,
+                    display: 'block'
+                })
+                .html($p);
+
+            setTimeout(function() {
+                $wrapper.css({display: 'none'});
+            }, timeout);
+        },
+
+        errorHandler: function(xhr) {
+            this.notification({message: xhr.errors});
+        },
+
+        renderNotifications: function() {
+            this.$notificationsWrapper = $('<div id="euexNotificationWrapper"></div>');
+            this.$notificationsWrapper.css({
+                position       : 'fixed',
+                top            : 0,
+                width          : '100%',
+                background     : '#fff',
+                //'border-bottom': '5px solid red',
+                padding        : '30px',
+                'z-index'      : 100,
+                'display'      : 'none'
+            });
+
+            $('body').append(this.$notificationsWrapper);
+        },
+
+        renderButton: function() {
+            var $actions = $('.pv-top-card-section__actions');
+            var $span;
+
+            if ($actions.find('.hunterBtn').length) {
+                this.$btn = $actions.find('.hunterBtn');
+                this.$btn.on('click', $.proxy(this.onHunterClick, this)); // on click
+
+                return;
+            }
+
+            this.$btn = $([
+                '<button class="primary top-card-action" style="background: #FF5722;">',
+                '<span class="btnText hunterBtn">Hunter</span>',
+                '</button>'
+            ].join(' '));
+            this.$btn.on('click', $.proxy(this.onHunterClick, this));     // on click
+
+            if ($actions.find('.primary.message').length) {
+                $span = $('<span data-control-name="euex-message" class="inline-block"></span>');
+                $span.html(this.$btn);
+                $actions.append($span);
+            } else {
+                $actions.append(this.$btn);
+            }
+        },
+
+        render: function() {
+            this.renderButton();
+            this.renderNotifications();
+        }
+    };
+
+    $(document).ready(function () {
+        var _path;
+        var hunter;
+
+        setInterval(function() {
+            if (window.location.pathname !== _path) {
+                _path = window.location.pathname;
+                console.log('>>> changed path', _path);
+
+                if (PATTERNS.PROFILE_URL.test(_path)) {
+                    hunter = new ProfileHunter();
+                }
+            }
+
+        }, 200);
+    });
+
 })();
 
-SOCIAL_PARSER.onLoadProfile(function (err) {
+/*SOCIAL_PARSER.onLoadProfile(function (err) {
     var data;
 
     if (err) {
@@ -204,11 +386,10 @@ SOCIAL_PARSER.onLoadProfile(function (err) {
     data = SOCIAL_PARSER.parseProfile();
     console.log('>>> data', data);
     console.log('>>> data', JSON.stringify(data));
-});
+});*/
 
 /*SOCIAL_PARSER.onLoadJobs(function() {
  var data = SOCIAL_PARSER.parseJobs();
 
  console.log('>>> data', data);
  });*/
-
