a5d800e6746db5754085f03360f09d1bf91773a5
nazarovits <istvan.nazarovits@gmail.com>
2017-06-16 14:35:20 +0200

    Added grunt task for build compression

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,20 @@
 module.exports = function(grunt) {
+    function getBuildName() {
+        var now = new Date();
+        var dateStr = grunt.template.date(now, 'ddmmyy');
+        var version = grunt.option('ver') || '1.0';
+        var sha = (grunt.option('sha')) ? 'SHA' + grunt.option('sha') : '';
+        var name;
+
+        name = ['build_full', 'v1.'+version, dateStr, sha + '.zip'].join('_');
+        console.log('>>> name', name);
+
+        return name;
+    }
+
     grunt.initConfig({
         watch: {
             scripts: {
-                //files: ['public/js/*.js'],
                 files: ['public/**/*'],
                 tasks: ['clear', 'copy:dev']
             }
@@ -10,37 +22,34 @@ module.exports = function(grunt) {
         copy: {
             main: {
                 files: [
-                    // includes files within path
-                    //{expand: true, cwd: 'public/', src: ['js/libs/**', 'js/popup.js'], dest: 'build/'},
                     {expand: true, src: ['manifest.json', 'icon.png'], dest: 'build/'},
-                    {expand: true, cwd: 'public/', src: ['popup.html', 'options.html'], dest: 'build/'},
+                    {expand: true, cwd: 'public/', src: ['popup.html'], dest: 'build/'},
                     {expand: true, cwd: 'public/', src: ['js/**', 'css/**', 'fonts/**', 'img/**'], dest: 'build/'}
-
-                    // includes files within path and its sub-directories
-                    /*{expand: true, src: ['path/!**'], dest: 'dest/'},
-
-                    // makes all src relative to cwd
-                    {expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
-
-                    // flattens results to a single level*/
-                    //{expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},
                 ]
             },
             dev: {
                 files: [
                     {expand: true, src: ['manifest.json', 'icon.png'], dest: 'build/'},
-                    {expand: true, cwd: 'public/', src: ['popup.html', 'options.html'], dest: 'build/'},
+                    {expand: true, cwd: 'public/', src: ['popup.html'], dest: 'build/'},
                     {expand: true, cwd: 'public/', src: [
                         'js/parser.js',
                         'js/euexContentScript.js',
                         'js/contentScript.js',
                         'js/config.js',
-                        'js/templates.js',
                         'js/api.js',
                         'js/popup.js',
-                        'js/background.js',
-                        'js/options.js'
+                        'js/background.js'
                     ], dest: 'build/'}
+                ]
+            }
+        },
+        compress: {
+            main: {
+                options: {
+                    archive: 'builds/' + getBuildName()
+                },
+                files: [
+                    {src: ['build/**'], dest: 'builds/'}
                 ]
             }
         }
@@ -50,9 +59,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-clear');
+    grunt.loadNpmTasks('grunt-template');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
     // register grunt tasks:
-    grunt.registerTask('build', ['copy:main']);
+    grunt.registerTask('build', ['copy:main', 'compress']);
     grunt.registerTask('build-dev', ['copy:dev']);
     grunt.registerTask('default', ['build']);
 };
