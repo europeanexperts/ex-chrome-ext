d0005bfddc5859ea6650ae524b5b170adec4a738
Alex Roman <alex.roman@thinkmobiles.com>
2017-11-21 16:58:42 +0200

    merge templates.js

---
+++ public/js/templates.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict';
+
+  var compiledTemplates = {};
+  var $el = $('#templates');
+
+  var _TEMPLATES = {
+    getTemplate: function(templateId) {
+      var $template;
+
+      if (!compiledTemplates[templateId]) {
+        $template = $el.find('#' + templateId + '-template');
+        compiledTemplates[templateId] = _.template($template.html());
+      }
+
+      return compiledTemplates[templateId];
+    }
+  };
+
+  window.APP_TEMPLATES = window.APP_TEMPLATES || _TEMPLATES;
+})();
